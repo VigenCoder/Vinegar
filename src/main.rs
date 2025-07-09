d61906109d82c5eaaec45eaaefa35a0091b09b95
Peter Yuan <ywx2006@outlook.com>
2025-07-09 21:27:59 +0800

    feat: add basic cli (#2)
    * chore: add clap dependency
    
    add `clap` dependency in Cargo.toml
    and accordingly update Cargo.lock
    
    * feat: add basic cli supporting stdio

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,59 @@
+use clap::{self, Parser};
 mod codec;
 
 use crate::codec::{Decodee, Encodee};
 
+#[derive(Parser)]
+#[command(
+    name = "vinegar",
+    version = "0.1.0",
+    about = "A simple command line byte stream codec tool"
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(clap::Subcommand)]
+enum Commands {
+    #[command(about = "encode data using the codec")]
+    Encode,
+    #[command(about = "decode data using the codec")]
+    Decode,
+    #[command(about = "example encoding and decoding")]
+    Example,
+}
+
 fn main() {
-  let text = "你好{He\u{4e16}llo} 🦀界Wo\nrld！";
-  println!("{}", text);
-  let mut encoder = Encodee::new(text.to_string(), 5055, "Vigen".to_string());
-  println!("{}", encoder.encode());
-  let mut decoder = Decodee::new(encoder.encode().to_string(), 5055, "Vigen".to_string());
-  println!("{}", decoder.decode());
+    let cli = Cli::parse();
+    let mut buffer: String = String::new();
+
+    match &cli.command {
+        Commands::Encode => {
+            std::io::stdin()
+                .read_line(&mut buffer)
+                .expect("Failed to read from stdin");
+            let mut encoder = Encodee::new(buffer, 5055, String::from("Vigen"));
+            let encoded = encoder.encode();
+            println!("{}", encoded);
+        }
+        Commands::Decode => {
+            std::io::stdin()
+                .read_line(&mut buffer)
+                .expect("Failed to read from stdin");
+            buffer = buffer.trim_end().to_string();
+
+            let mut decoder = Decodee::new(buffer, 5055, String::from("Vigen"));
+            let decoded = decoder.decode();
+            println!("{}", decoded);
+        }
+        Commands::Example => {
+            let example = "你好{He\u{4e16}llo} 🦀界Wo\nrld！";
+            println!("Example: \n{:?}", example);
+            let mut encoder = Encodee::new(example.to_string(), 5055, "Vigen".to_string());
+            println!("Encoded: \n{:?}", encoder.encode());
+            let mut decoder = Decodee::new(encoder.encode().to_string(), 5055, "Vigen".to_string());
+            println!("Decoded: \n{:?}", decoder.decode());
+        }
+    }
 }
