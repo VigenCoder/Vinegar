692e57e85da728a09f2ad9771387647c584d799c
xtz206 <ywx2006@outlook.com>
2025-07-15 21:31:55 +0800

    feat: add basic file io

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::{self, Parser};
+use std::io::{Read, Write};
+use std::path::PathBuf;
 mod codec;
 
 use crate::codec::{Decodee, Encodee};
@@ -12,6 +14,11 @@ use crate::codec::{Decodee, Encodee};
 struct Cli {
     #[command(subcommand)]
     command: Commands,
+
+    #[arg(short, long, help = "input file path")]
+    input: Option<PathBuf>,
+    #[arg(short, long, help = "output file path")]
+    output: Option<PathBuf>,
 }
 
 #[derive(clap::Subcommand)]
@@ -30,22 +37,54 @@ fn main() {
 
     match &cli.command {
         Commands::Encode => {
-            std::io::stdin()
-                .read_line(&mut buffer)
-                .expect("Failed to read from stdin");
+            if let Some(path) = &cli.input {
+                let mut file = std::fs::File::open(path).expect("Failed to open input file");
+                file.read_to_string(&mut buffer)
+                    .expect("Failed to read from input file");
+            } else {
+                std::io::stdin()
+                    .read_to_string(&mut buffer)
+                    .expect("Failed to read from stdin");
+            }
+
+            buffer = buffer.trim_end().to_string();
             let mut encoder = Encodee::new(buffer, 5055, String::from("Vigen"));
             let encoded = encoder.encode();
-            println!("{}", encoded);
+
+            if let Some(path) = &cli.output {
+                let mut file = std::fs::File::create(path).expect("Failed to create output file");
+                file.write_all(encoded.as_bytes())
+                    .expect("Failed to write to output file");
+            } else {
+                std::io::stdout()
+                    .write_all(encoded.as_bytes())
+                    .expect("Failed to write to stdout");
+            }
         }
         Commands::Decode => {
-            std::io::stdin()
-                .read_line(&mut buffer)
-                .expect("Failed to read from stdin");
-            buffer = buffer.trim_end().to_string();
+            if let Some(path) = &cli.input {
+                let mut file = std::fs::File::open(path).expect("Failed to open input file");
+                file.read_to_string(&mut buffer)
+                    .expect("Failed to read input file");
+            } else {
+                std::io::stdin()
+                    .read_to_string(&mut buffer)
+                    .expect("Failed to read stdin");
+            }
 
+            buffer = buffer.trim_end().to_string();
             let mut decoder = Decodee::new(buffer, 5055, String::from("Vigen"));
             let decoded = decoder.decode();
-            println!("{}", decoded);
+
+            if let Some(path) = &cli.output {
+                let mut file = std::fs::File::create(path).expect("Failed to create output file");
+                file.write_all(decoded.as_bytes())
+                    .expect("Failed to write to output file");
+            } else {
+                std::io::stdout()
+                    .write_all(decoded.as_bytes())
+                    .expect("Failed to write to stdout");
+            }
         }
         Commands::Example => {
             let example = "你好{He\u{4e16}llo} 🦀界Wo\nrld！";
